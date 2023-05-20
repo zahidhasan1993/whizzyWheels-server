6c4291d85af4678b7b1dbff81ff6ed31fe1e13f4
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-21 00:11:14 +0600

    added post route for addtoy from clint side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -30,15 +30,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("whizzywheels")
-    const categoryToyCollections = database.collection("toy-category");
+    const database = client.db("whizzywheels");
+    const categoryToyCollections = database.collection('all-toys');
 
     //server db routes
-
-    app.get('/toycategory',async (req,res) => {
+    // get db
+    app.get('/alltoys',async (req,res) => {
       const cursor = categoryToyCollections.find();
       const result = await cursor.toArray();
       res.send(result)
+    })
+    app.get('/alltoys/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await categoryToyCollections.findOne(query);
+
+      res.send(result)
+    })
+    //insert to db
+    app.post('/addtoy', async (req,res) => {
+        const newToy = req.body;
+        const result = await categoryToyCollections.insertOne(newToy);
+        res.send(result)
+        // console.log(newToy);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
