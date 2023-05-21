e8d3c702ea5d232c4ea1061971bfd7a98dfc9f1f
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-21 18:29:04 +0600

    completing the project

--- index.js
+++ index.js
@@ -35,10 +35,19 @@ async function run() {
     //server db routes
     // get db
     app.get('/alltoys',async (req,res) => {
+      
       const cursor = toyCollections.find();
       const result = await cursor.toArray();
       res.send(result)
     })
+    //sort db
+    app.get('/sort',async (req,res) => {
+      
+      const cursor = toyCollections.find();
+      const result = await cursor.sort({price : -1}).toArray();
+      res.send(result)
+    })
+    //get by id
     app.get('/alltoys/:id', async (req,res) => {
       const id = req.params.id;
       const query = { _id : new ObjectId(id)};
@@ -80,6 +89,7 @@ async function run() {
 
       res.send(result)
     })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
